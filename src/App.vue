c858cdc24ee1e733266461f9acf4b8a01f46768e
snfz <winteriscoming@qq.com>
2021-03-25 20:23:36 +0800

    format

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-    <div id="app">
-        <faceContent></faceContent>
-    </div>
+  <div id="app">
+    <faceContent></faceContent>
+  </div>
 </template>
 
 <script>
 import faceContent from './components/faceContent'
 
 export default {
-    name: 'App',
-    components: {
-        faceContent,
-    },
+  name: 'App',
+  components: {
+    faceContent,
+  },
 }
 </script> 
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
